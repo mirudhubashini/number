208fc38437738560156a82233e99779e1a76c2d9
Admin <Admin@192.168.1.32>
2019-05-14 18:42:15 +0530

    three syso was added

--- src/number/Javaone.java
+++ src/number/Javaone.java
@@ -4,5 +4,6 @@ public class Javaone {
 	public static void main(String[] args) {
 		System.out.println("hi one!!!");
 		System.out.println("Hi two..!!!");
+		System.out.println("Hi three..!!!");
 	}
 }
